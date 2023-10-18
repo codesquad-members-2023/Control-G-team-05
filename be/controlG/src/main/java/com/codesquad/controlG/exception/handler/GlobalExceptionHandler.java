fe5627bd1bfeb3ebdada84861da65d76a3f0f0aa
DOEKYONG <apochmn@naver.com>
2023-10-18 18:15:05 +0900

    #16 [BE] feat: 이미지 업로드 기능 추가

--- be/controlG/src/main/java/com/codesquad/controlG/exception/handler/GlobalExceptionHandler.java
+++ be/controlG/src/main/java/com/codesquad/controlG/exception/handler/GlobalExceptionHandler.java
@@ -3,9 +3,11 @@ package com.codesquad.controlG.exception.handler;
 import com.codesquad.controlG.exception.CustomRuntimeException;
 import java.util.Map;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
 
 @RestControllerAdvice
 @Slf4j
@@ -16,6 +18,14 @@ public class GlobalExceptionHandler {
         log.info("api 예외발생! errorType: " + e.getCustomException().getName() + " errorMessage: " + e.getCustomException()
                 .getErrorMessage());
         return e.sendError();
+    }
+
+    @ExceptionHandler(MaxUploadSizeExceededException.class)
+    public ResponseEntity<Map<String, String>> maxUploadSizeExceededException(MaxUploadSizeExceededException e) {
+        log.info("api 예외발생! errorType: " + e.getClass().getSimpleName() + " errorMessage: " + "최대 파일 업로드 용량 초과");
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(Map.of("errorType", e.getClass().getSimpleName(), "errorMessage",
+                        "최대 파일 사이즈를 초과합니다."));
     }
 
 }
