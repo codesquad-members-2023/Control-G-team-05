22964f196e7fcfcb8161a0bedd252ed9776514d7
CDBchan <kdjin1215@naver.com>
2023-10-19 15:21:06 +0900

    #31 [FE] feat: 그룹 상세페이지 구성

--- fe/src/component/js/TopBarWithCenterText.js
+++ fe/src/component/js/TopBarWithCenterText.js
@@ -8,7 +8,7 @@ function TopBarWithCenterText({ topBarName }) {
         <img
           alt=""
           src="https://d1xzdqg8s8ggsr.cloudfront.net/652ca67bbbe533c504a77c20/60605814-9ca5-44e6-b700-18549a8bf841_1697439260687989733?Expires=-62135596800&Signature=CBTQwaMvZSKES8D8Zr5MYEBGYP4pFQrBe68ZDBihMsaRWbHXmJPNiuUN-r94WT9wXhTB89NbkfVvydtHrFWy80U-tRiNrVcVman5OWJkkSIzBOQLoHEh1Lee~ETslcXyxCohirUXlv-x2GqfQ6bxR2GKIej04tYJpOOq4V8koLVwc7sJSnbhGm5~wlB6PAajIsoTWcyTdo33eNjVwRMuPGk8nIh149b2gkkIJEaKRbcSVNr1BHsX4BcYjTiecUG2SYB6wtOrkv2mhOdJIxMOSUdKIL9XvNIL~6O2ftgqIkLMGCg~7~WX1zmFVkgJOM76MXrwAEAiVnNSGMwSMhiyyw__&Key-Pair-Id=K1P54FZWCHCL6J"
-          className={styles.notificationsNoneIcon}
+          className={styles.backButton}
         />
       </Link>
       <div className={styles.topBarwrapper}>
---
+++ fe/src/routes/js/GroupDetailPage.js
@@ -0,0 +1,20 @@
+import NavigationBar from "../../component/js/NavigationBar";
+import TopBarWithCenterText from "../../component/js/TopBarWithCenterText";
+import GroupProfileInformation from "../../component/js/GroupProfileInformation";
+import styles from "../css/GroupDetailPage.module.css";
+
+function GroupDetailPage() {
+  return (
+    <div className={styles.div}>
+      <TopBarWithCenterText topBarName="Group Profile" />
+      <GroupProfileInformation
+        groupImgSrc="https://d1xzdqg8s8ggsr.cloudfront.net/652ca67bbbe533c504a77c20/006135da-e13c-46d3-89c3-88b15d7a8696_1697439278585209072?Expires=-62135596800&Signature=Y9O5jErsMQ~mtiybMdmEijNxIA1FgtLH14QB9zsK5W9TfRVAtkkpbfZY6giEgPJKh643MKvdFixFIsEssuIKQelZi46Xan6Oszjj6QukzGZ61E0oMINjGMBZd9td98dq7sJNNnP0mHbAHzZunf2AMHkDYvs05n8SvrkDwlWeu99UV~gR~~Kunnnku8fqmPOMKnujFCNmpqhfiVJp0Pm~wi1GolBzCOB6bFO52LA7PpLbL-FNHGMk5S~irGbyXb9T7zwfr1JpkOJWSD9YF4twgWG0e3WomrLt4V7Ouc0zyeRLJEuXE55P9Uof899M1eGE18WVVM5Vt29EwLw745DhQQ__&Key-Pair-Id=K1P54FZWCHCL6J"
+        groupName="Code Squad"
+        memberCount="5.3"
+      />
+      <NavigationBar />
+    </div>
+  );
+}
+
+export default GroupDetailPage;
