6e5028c9c82b5a30fcf68e639dac9aab25feb598
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2023-10-23 14:51:33 +0900

    #45 [BE] test: 그룹 api 테스트 작성 (#46)

---
+++ be/controlG/src/test/java/com/codesquad/controlG/annotation/AcceptanceTestSupport.java
@@ -0,0 +1,25 @@
+package com.codesquad.controlG.annotation;
+
+import com.codesquad.controlG.domain.auth.jwt.JwtProvider;
+import com.codesquad.controlG.domain.group.repository.GroupRepository;
+import com.codesquad.controlG.domain.member.repository.MemberRepository;
+import com.codesquad.controlG.domain.member_group.repository.MemberGroupRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@AcceptanceTest
+public abstract class AcceptanceTestSupport {
+
+    protected static final String JWT_TOKEN_PREFIX = "Bearer ";
+
+    @Autowired
+    protected JwtProvider jwtProvider;
+
+    @Autowired
+    protected GroupRepository groupRepository;
+
+    @Autowired
+    protected MemberRepository memberRepository;
+
+    @Autowired
+    protected MemberGroupRepository memberGroupRepository;
+}
