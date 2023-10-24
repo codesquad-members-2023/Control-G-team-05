746d463542d3f6170c75a376f90440a1883fccae
CDBchan <kdjin1215@naver.com>
2023-10-24 15:48:41 +0900

    #53 [FE] feat: 메인페이지 api 연동

--- fe/src/component/js/SearchInput.js
+++ fe/src/component/js/SearchInput.js
@@ -2,7 +2,12 @@ import React from "react";
 import TextField from "@mui/material/TextField";
 import styles from "../css/SearchInput.module.css";
 
-function SearchInput() {
+function SearchInput({ setSearchWord }) {
+  const handleInputChange = (event) => {
+    const value = event.target.value;
+    setSearchWord(value);
+  };
+
   return (
     <TextField
       className={styles.input}
@@ -10,6 +15,7 @@ function SearchInput() {
       label="Search"
       sx={{ width: 327 }}
       variant="standard"
+      onChange={handleInputChange}
     />
   );
 }
--- fe/src/constants/endpoints.js
+++ fe/src/constants/endpoints.js
@@ -2,5 +2,19 @@ export const BASE_URL = "http://localhost:8080";
 export const API_ENDPOINTS = {
   LOGIN: (queryString) =>
     `${BASE_URL}/api/login/oauth/naver?code=${queryString}`,
+
   SIGNUP: `${BASE_URL}/api/auth/signup`,
+
+  MEMBER_GROUP_LIST: (searchWord, memberId) => {
+    let queryString = "";
+
+    if (searchWord && memberId) {
+      queryString = `?word=${searchWord}&forMember=true}`;
+    } else if (searchWord) {
+      queryString = `?word=${searchWord}`;
+    } else if (memberId) {
+      queryString = `?forMember=true`;
+    }
+    return `${BASE_URL}/api/groups${queryString}`;
+  },
 };
