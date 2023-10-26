aea3dc4ec5c7b502830448c5024eff5d65cf5a5c
CDBchan <kdjin1215@naver.com>
2023-10-26 21:40:21 +0900

    #68 [FE] refactor: 채팅방 css 수정

--- fe/src/routes/js/ChatDetailPage.js
+++ fe/src/routes/js/ChatDetailPage.js
@@ -36,25 +36,36 @@ function ChatDetailPage() {
           time="2:49"
           isRead="Read"
         />
+        <ChatMessageBox
+          isOwnMessage={true}
+          message="bye charlie"
+          time="2:48"
+          isRead="Read"
+        />
+        <ChatMessageBox
+          isOwnMessage={false}
+          message="bye light"
+          time="2:49"
+          isRead="Read"
+        />
       </div>
       <div className={styles.chatDetailBottomBar}>
-        <div className={styles.messagecontainerParent}>
-          <TextField
-            color="primary"
-            label="Message"
-            sx={{
-              width: 325,
-            }}
-            variant="outlined"
-            multiline
-            maxRows={5}
-          />
-          <img
-            className={styles.sendIcon}
-            alt=""
-            src="https://d1xzdqg8s8ggsr.cloudfront.net/652ca67bbbe533c504a77c20/5cda7c3a-76da-4bcb-bdfa-a7e8a0e632f8_1697594684559098000?Expires=-62135596800&Signature=BIsAb0t1Bbl2E~Ndb~dB~WTBxhn69WWHHe4pAvdpJ6JlM-g7q-cZ~heHBXvFm~7d6-KWJ-NMFg854jMLhsxcXxOkVtUWBeUX7PNyUVtzpPrTfinVVLm-3j2SulgPNDgzwKeoL4zIbC9ZBmiwPe~bv-aPbBcOklRaF-2SAJzd3vO493nG2UOB49Oo-wrWgbyKSJCdoQufa4dDvus6V-w-ra5rbFNZPKYGAObvCw5et9GagYNFPRImOxDqjgHTZbc7zt7BZiVx0LLGbIYeyUXQtHlM~TxFgfB639N3Ic41IJMuKLCsNTLFRDldLthgnFWx~79hsKQWz5EPdPT~yS7KDQ__&Key-Pair-Id=K1P54FZWCHCL6J"
-          />
-        </div>
+        <TextField
+          color="primary"
+          label="Message"
+          sx={{
+            width: 320,
+          }}
+          size="small"
+          variant="outlined"
+          multiline
+          maxRows={3}
+        />
+        <img
+          className={styles.sendIcon}
+          alt=""
+          src="https://d1xzdqg8s8ggsr.cloudfront.net/652ca67bbbe533c504a77c20/5cda7c3a-76da-4bcb-bdfa-a7e8a0e632f8_1697594684559098000?Expires=-62135596800&Signature=BIsAb0t1Bbl2E~Ndb~dB~WTBxhn69WWHHe4pAvdpJ6JlM-g7q-cZ~heHBXvFm~7d6-KWJ-NMFg854jMLhsxcXxOkVtUWBeUX7PNyUVtzpPrTfinVVLm-3j2SulgPNDgzwKeoL4zIbC9ZBmiwPe~bv-aPbBcOklRaF-2SAJzd3vO493nG2UOB49Oo-wrWgbyKSJCdoQufa4dDvus6V-w-ra5rbFNZPKYGAObvCw5et9GagYNFPRImOxDqjgHTZbc7zt7BZiVx0LLGbIYeyUXQtHlM~TxFgfB639N3Ic41IJMuKLCsNTLFRDldLthgnFWx~79hsKQWz5EPdPT~yS7KDQ__&Key-Pair-Id=K1P54FZWCHCL6J"
+        />
       </div>
     </div>
   );
